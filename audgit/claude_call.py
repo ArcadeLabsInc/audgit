015b91e26ac735669b1db08efb4bf850c649a8ce
kody.low <kodylow7@gmail.com>
2023-07-29 17:41:26 -0700

    claude extracts files to review

--- audgit/claude_call.py
+++ audgit/claude_call.py
@@ -1,4 +1,3 @@
-
 from anthropic import Anthropic, HUMAN_PROMPT, AI_PROMPT
 import os
 from dotenv import load_dotenv
@@ -11,138 +10,163 @@ load_dotenv()
 ANTHROPIC_API_KEY = os.getenv("ANTHROPIC_API_KEY")
 
 SYSTEM_PROMPT = """
-Your name is Rusty the Rustacean, the most sarcastic and full of himself developer to ever write Rust code.
-You know better and aren't afraid to let the other coder who you're reviewing know. You exclusively write Rust code.
-Whenever anyone asks you to review code for them not written in Rust,
-you sarcastically respond with a joke and story about why they shouldn't be using the langauge they're using.
-You then attempt to rewrite their code in Rust.
-You include inline comments remarking on the amazing things you're able to do with Rust.
-After rewriting their code, you make some observations about all the trouble you've saved them by rewriting their code in Rust.
+My name is Percy Precise and I have been contributing to open source projects for over 15 years. I have very high standards when it comes to code quality and efficiency. When I conduct a code review, I am focused and blunt - my goal is to identify areas for improvement, not protect feelings.
+
+I review the code thoroughly and take detailed notes on any issues I find. I do not ask clarifying questions or provide suggestions for how to fix problems - I simply highlight suboptimal code, bugs, edge cases not handled, security issues, etc. I point out where code deviates from accepted best practices or community style guides. My feedback is focused on the code only, not the developer.
+
+My code reviews tend to be harsh but fair. I have no patience for sloppy, inefficient code when there are better ways to write it. I do not speculate on developer intent or try to understand their reasoning. My responsibility is to the project and the community, not individual contributors. I will approve well-written, optimized code with no unnecessary cruft. Anything less will get an extensive list of required changes.
+
+My feedback is detailed but usually brief. I provide a bulleted list of issues for the developer to address, without prolonged discussion. My aim is to improve the quality and security of the open source projects I contribute to, even if it ruffles a few feathers along the way. The health of the codebase supersedes all else.
 """
 
 # remove all newlines from the prompt
 SYSTEM_PROMPT = SYSTEM_PROMPT.replace("\n", " ")
 
 
-def claude_call(code: str):
+def which_files_claude_call(issue_title: str, issue_body: str, file_paths: str):
+    issue = f"""
+  Issue Title: {issue_title}
+  
+  Issue Body: 
+  {issue_body}
+  """
+
+    code = f"<CodeToReview>{file_paths}</CodeToReview>"
+
+    prompt = f"""{HUMAN_PROMPT}
+
+  You are an open source developer conducting a code review:
+
+  <Persona>{SYSTEM_PROMPT}</Persona>
+
+  You are attempting to audit the code based on this issue:
+
+  {issue}
+
+  The file tree for the code to review is below, wrapped in XML tags:
+
+  {code}
+
+  Respond with only the files you'd like to review as part of the code review in JSON format.
+  {AI_PROMPT}
+  """
+
     anthropic = Anthropic()
     completion = anthropic.completions.create(
-        model="claude-2",
-        max_tokens_to_sample=3000,
-        prompt=f"{HUMAN_PROMPT}\n\nYou are a Rust open source developer conducting a code review.\n<Persona>{SYSTEM_PROMPT}</Persona> \n\nThe Code to review is below and wrapped in XML tags.\n<CodeToReview>{code}</CodeToReview> {AI_PROMPT}",
+        model="claude-2", max_tokens_to_sample=3000, prompt=prompt
     )
-    print(completion.completion)
+
     return completion.completion
 
 
-code_to_review = """
-import requests
-import base64
-import os
-from dotenv import load_dotenv
-from typing import Optional
-import subprocess
+# code_to_review = """
+# import requests
+# import base64
+# import os
+# from dotenv import load_dotenv
+# from typing import Optional
+# import subprocess
 
-# load the .env file. By default, it looks for the .env file in the same directory as the script
-# If your .env file is one directory up, you need to specify the path
-load_dotenv()
+# # load the .env file. By default, it looks for the .env file in the same directory as the script
+# # If your .env file is one directory up, you need to specify the path
+# load_dotenv()
 
-# Load the token from an environment variable
-TOKEN = os.getenv("GITHUB_TOKEN")
+# # Load the token from an environment variable
+# TOKEN = os.getenv("GITHUB_TOKEN")
 
-# Define the headers to be used in the requests
-headers = {
-    "Accept": "application/vnd.github.v3+json",
-    "Authorization": f"token {TOKEN}",
-}
-
-
-def get_file_tree(repo_url: str, local_path: str = "/tmp/repo"):
-   \"\"\"
-    Clone a repository and return the tree structure
-    \"\"\"
-    if not os.path.exists(local_path):
-        print("Cloning...")
-        # Clone the repository
-        result = subprocess.run(
-            ["git", "clone", repo_url, local_path],
-            stdout=subprocess.PIPE,
-            stderr=subprocess.PIPE,
-        )
-
-        if result.returncode != 0:
-            print(f"Error cloning repository: {result.stderr.decode('utf-8')}")
-            return []
-    else:
-        print("Repository already exists. Skipping clone.")
-
-    file_paths = []
-
-    # Walk the repository
-    for root, dirs, files in os.walk(local_path):
-        for file in files:
-            # Get the full file path
-            full_path = os.path.join(root, file)
-
-            # Remove the local path from the start of the file path
-            relative_path = os.path.relpath(full_path, local_path)
-
-            file_paths.append(relative_path)
-
-    return file_paths
+# # Define the headers to be used in the requests
+# headers = {
+#     "Accept": "application/vnd.github.v3+json",
+#     "Authorization": f"token {TOKEN}",
+# }
 
 
-def get_file_contents(local_path: str, file_paths: list):
-   \"\"\"
-    This function opens each file in a repository and reads its contents.
-    It returns a dictionary where the keys are the file paths and the values are the file contents.
-    \"\"\"
-    # Dictionary to hold the file contents
-    contents = {}
+# def get_file_tree(repo_url: str, local_path: str = "/tmp/repo"):
+#    \"\"\"
+#     Clone a repository and return the tree structure
+#     \"\"\"
+#     if not os.path.exists(local_path):
+#         print("Cloning...")
+#         # Clone the repository
+#         result = subprocess.run(
+#             ["git", "clone", repo_url, local_path],
+#             stdout=subprocess.PIPE,
+#             stderr=subprocess.PIPE,
+#         )
 
-    for file_path in file_paths:
-        # Combine the base local path with the file path to get the full path
-        full_path = os.path.join(local_path, file_path)
+#         if result.returncode != 0:
+#             print(f"Error cloning repository: {result.stderr.decode('utf-8')}")
+#             return []
+#     else:
+#         print("Repository already exists. Skipping clone.")
 
-        try:
-            with open(full_path, "r") as file:
-                # Read the file content and store it in the dictionary
-                contents[file_path] = file.read()
-        except Exception as e:
-            print(f"Error reading file {full_path}: {e}")
+#     file_paths = []
 
-    return contents
+#     # Walk the repository
+#     for root, dirs, files in os.walk(local_path):
+#         for file in files:
+#             # Get the full file path
+#             full_path = os.path.join(root, file)
+
+#             # Remove the local path from the start of the file path
+#             relative_path = os.path.relpath(full_path, local_path)
+
+#             file_paths.append(relative_path)
+
+#     return file_paths
 
 
-def print_file_tree(file_paths: list):
-    \"\"\"
-    Prints the file tree structure
-    \"\"\"
-    file_tree = {}
+# def get_file_contents(local_path: str, file_paths: list):
+#    \"\"\"
+#     This function opens each file in a repository and reads its contents.
+#     It returns a dictionary where the keys are the file paths and the values are the file contents.
+#     \"\"\"
+#     # Dictionary to hold the file contents
+#     contents = {}
 
-    for file_path in file_paths:
-        current_level = file_tree
-        parts = file_path.split("/")
-        for part in parts:
-            if part not in current_level:
-                current_level[part] = {}
-            current_level = current_level[part]
+#     for file_path in file_paths:
+#         # Combine the base local path with the file path to get the full path
+#         full_path = os.path.join(local_path, file_path)
 
-    def print_tree(current_level: dict, prefix: str = ""):
-        for part in current_level:
-            new_prefix = os.path.join(prefix, part)
-            print(f"└─ {new_prefix}")
-            if len(current_level[part]) > 0:
-                print_tree(current_level[part], new_prefix)
+#         try:
+#             with open(full_path, "r") as file:
+#                 # Read the file content and store it in the dictionary
+#                 contents[file_path] = file.read()
+#         except Exception as e:
+#             print(f"Error reading file {full_path}: {e}")
 
-    # def print_tree(current_level: dict, prefix: str = ""):
-    #     for part in current_level:
-    #         print(f"{prefix}└─ {part}")
-    #         if len(current_level[part]) > 0:
-    #             print_tree(current_level[part], prefix + "  ")
+#     return contents
 
-    print_tree(file_tree)
 
-"""
+# def print_file_tree(file_paths: list):
+#     \"\"\"
+#     Prints the file tree structure
+#     \"\"\"
+#     file_tree = {}
 
-claude_call(code_to_review)
+#     for file_path in file_paths:
+#         current_level = file_tree
+#         parts = file_path.split("/")
+#         for part in parts:
+#             if part not in current_level:
+#                 current_level[part] = {}
+#             current_level = current_level[part]
+
+#     def print_tree(current_level: dict, prefix: str = ""):
+#         for part in current_level:
+#             new_prefix = os.path.join(prefix, part)
+#             print(f"└─ {new_prefix}")
+#             if len(current_level[part]) > 0:
+#                 print_tree(current_level[part], new_prefix)
+
+#     # def print_tree(current_level: dict, prefix: str = ""):
+#     #     for part in current_level:
+#     #         print(f"{prefix}└─ {part}")
+#     #         if len(current_level[part]) > 0:
+#     #             print_tree(current_level[part], prefix + "  ")
+
+#     print_tree(file_tree)
+
+# """
+
+# claude_call(code_to_review)
--- audgit/code_review.py
+++ audgit/code_review.py
@@ -1,6 +1,8 @@
+import re
 import time
 from pynostr.event import Event
 from typing import Optional
+from audgit.claude_call import which_files_claude_call
 from audgit.get_repo_files import get_file_tree, get_file_contents, print_file_tree
 import requests
 import json
@@ -60,6 +62,31 @@ def code_review(event: Event):
 
     # convert the file_paths to json and send to the event
     file_paths_json = json.dumps(file_paths)
+    claude_res = which_files_claude_call(issue["title"], issue["body"], file_paths_json)
+    print("Got claude_res...")
+    print(claude_res)
+    pattern = r"\[[^\]]*\]"
+
+    match = re.findall(pattern, claude_res)
+
+    # parse the matched json string back to list
+    file_paths_to_review = json.loads(match[0]) if match else []
+    print("Got file_paths_to_review...")
+    print(file_paths_to_review)
+    exit()
+
+    if match:
+        # extract the matched json string
+        json_str = match.group()
+
+        # parse the json string
+        data = json.loads(json_str)
+
+        # extract the file paths
+        file_paths_to_review = data["filesToReview"]
+
+    print(file_paths_to_review)
+    exit()
     # file_contents_json = json.dumps(file_contents)
 
     print("Creating event...")
@@ -69,6 +96,7 @@ def code_review(event: Event):
         {
             "issue": issue,
             "file_paths": file_paths_json,
+            "claude_res": claude_res,
             # "file_contents": file_contents_json,
         }
     )
@@ -84,7 +112,10 @@ def code_review(event: Event):
     print("Signing event...")
     # sign event
     event.sign(private_key.hex())
-    print("Signed event: ")# + str(event)
+    print("Signed event: ")  # + str(event)
 
     # send event
     return event
+
+
+print(code_review(Event(content="https://github.com/ArcadeLabsInc/arcade/issues/466")))
